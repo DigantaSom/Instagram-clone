6d4291120db894d46fa30f1c6728f9dff99fdc2e
Diganta Som <itsdsom111@gmail.com>
2024-01-19 00:03:40 +0530

    Backend - Configuring CORS

--- api/src/main/java/com/instagram/api/config/AppConfig.java
+++ api/src/main/java/com/instagram/api/config/AppConfig.java
@@ -10,12 +10,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 public class AppConfig {
   @Bean
   public SecurityFilterChain securityConfiguration(HttpSecurity httpSecurity) throws Exception {
     httpSecurity
+        .cors(Customizer.withDefaults()) // by default, use a Bean by the name of 'corsConfigurationSource'
         .csrf(csrf -> csrf.disable())
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/signup")
@@ -29,8 +35,19 @@ public class AppConfig {
   }
 
   @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setExposedHeaders(List.of(SecurityContext.HEADER));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
+  @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
 }
- 
